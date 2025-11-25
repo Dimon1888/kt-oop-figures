a5783b689f98d2ac5aaa8bdebc25e2c1cf48f931
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2025-11-25 18:23:24 +0100

    I fixed the errors2

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -3,18 +3,18 @@ package mate.academy
 import mate.academy.abstract.Figure
 import mate.academy.service.FigureSupplier
 
+private const val TOTAL_FIGURES = 6
+
 fun main() {
     val figureSupplier = FigureSupplier()
-    val totalFigures = 6
     val figures = mutableListOf<Figure>()
 
-
-    val randomCount = totalFigures / 2
+    val randomCount = TOTAL_FIGURES / 2
     repeat(randomCount) {
         figures.add(figureSupplier.getRandomFigure())
     }
 
-    val defaultCount = totalFigures - randomCount
+    val defaultCount = TOTAL_FIGURES - randomCount
     repeat(defaultCount) {
         figures.add(figureSupplier.getDefaultFigure())
     }
